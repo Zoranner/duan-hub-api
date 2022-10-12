793c1cc116741b66e8d18229f42098da53f056a7
yangxinran <yangxinran@kimo-tech.com>
2022-10-12 18:58:56 +0800

    新增仿真工程实体类

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,58 @@
-export class Project {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+interface Condition {
+  target: string;
+  action: number;
+  timeLimit: number;
+}
+
+interface Goal {
+  type: number;
+  condition: Condition;
+}
+
+interface Option {
+  name: string;
+  current: any;
+}
+
+interface Node {
+  id: number;
+  sceneId: string;
+  name: string;
+  options: Option[];
+}
+
+interface Role {
+  id: number;
+  name: string;
+  control: string[];
+}
+
+class CampScript {
+  detail: string;
+  goals: Goal[];
+  nodes: Node[];
+  roles: Role[];
+}
+
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'json',
+  })
+  red: CampScript;
+
+  @Column({
+    type: 'json',
+  })
+  blue: CampScript;
+}
+
+export { CampScript };
