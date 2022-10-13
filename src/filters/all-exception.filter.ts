4817b18aafe9fcffb16cbf6e707096abe84d04d0
yangxinran <yangxinran@kimo-tech.com>
2022-10-13 17:16:48 +0800

    添加错误过滤器非HTTP错误的消息

--- src/filters/all-exception.filter.ts
+++ src/filters/all-exception.filter.ts
@@ -5,19 +5,17 @@ import { HttpAdapterHost } from '@nestjs/core';
 export class AllExceptionsFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
-  catch(exception: unknown, host: ArgumentsHost): void {
+  catch(exception: Error, host: ArgumentsHost): void {
     // In certain situations `httpAdapter` might not be available in the
     // constructor method, thus we should resolve it here.
     const { httpAdapter } = this.httpAdapterHost;
-
     const ctx = host.switchToHttp();
     const httpException = exception instanceof HttpException;
     const httpStatus = httpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
-    const errorMessage = httpException ? exception.message : 'Internal server error';
 
     const responseBody = {
       code: -1,
-      message: errorMessage,
+      message: exception.message,
     };
 
     httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
