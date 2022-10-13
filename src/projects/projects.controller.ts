06d5a6bd0dcaf6c7e0fc15c0128d8e0e14d91ee0
yangxinran <yangxinran@kimo-tech.com>
2022-10-13 17:22:16 +0800

    新增仿真工程分页查询接口

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Logger, Query, ForbiddenException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Logger, Query } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { ValidateLimitPipe, ValidatePagePipe } from 'src/utils/pipe/list.pipe';
 
 @Controller('projects')
 export class ProjectsController {
@@ -9,19 +10,14 @@ export class ProjectsController {
 
   constructor(private readonly projectsService: ProjectsService) {}
 
-  @Get('hello')
-  helloProject() {
-    return this.projectsService.hello();
-  }
-
   @Post('create')
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectsService.create(createProjectDto);
   }
 
   @Get('list')
-  findAll() {
-    return this.projectsService.findAll();
+  findAllWithPage(@Query('page', ValidatePagePipe) page: number, @Query('limit', ValidateLimitPipe) limit: number) {
+    return this.projectsService.findAllWithPage(page, limit);
   }
 
   @Get('detail')
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -14,24 +14,25 @@ export class ProjectsService {
     private readonly projectsRepository: Repository<Project>,
   ) {}
 
-  hello() {
-    const project: Project = new Project();
-    project.name = '第三次世界大战太空军博弈对抗';
-    project.red = new CampScript();
-    project.red.detail =
-      '第三次世界大战，为继第二次世界大战后未来可能爆发的下一次世界大战，是目前仅处于幻想中而尚未爆发的假想战争。但常见于政治论题、大众文化、民众之间茶余饭后的聊天内容里。该术语至少早在1941年就开始使用，有些角度将其宽其术语应用于有限的或更轻微的国际冲突，例如冷战或反恐战争。相比之下，其他角度则假想战争从破坏性影响上都将超过以前的世界大战。';
-    project.blue = new CampScript();
-    project.blue.detail =
-      '第三次世界大战，为继第二次世界大战后未来可能爆发的下一次世界大战，是目前仅处于幻想中而尚未爆发的假想战争。但常见于政治论题、大众文化、民众之间茶余饭后的聊天内容里。该术语至少早在1941年就开始使用，有些角度将其宽其术语应用于有限的或更轻微的国际冲突，例如冷战或反恐战争。相比之下，其他角度则假想战争从破坏性影响上都将超过以前的世界大战。';
-    return this.projectsRepository.save(project);
-  }
-
   create(createProjectDto: CreateProjectDto) {
     return this.projectsRepository.save(createProjectDto);
   }
 
-  findAll() {
-    return `This action returns all projects`;
+  async findAllWithPage(page: number, limit: number) {
+    const pageList = await this.projectsRepository.find({
+      select: {
+        id: true,
+        name: true,
+        createTime: true,
+      },
+      order: {
+        createTime: 'DESC',
+      },
+      skip: limit * (page - 1),
+      take: limit,
+    });
+
+    return { data: pageList, page: page, limit: limit };
   }
 
   async findOne(id: number) {
