6fc6eacbfeecb2a70a26a830e679d037ccc94c04
yangxinran <yangxinran@kimo-tech.com>
2022-10-18 10:32:40 +0800

    完善仿真历史记录实体类

--- src/histories/entities/history.entity.ts
+++ src/histories/entities/history.entity.ts
@@ -1,6 +1,12 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
+import { Project } from 'src/projects/entities/project.entity';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export class History {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({
+    comment: '唯一标识',
+  })
   id: number;
+
+  @Column()
+  project: Project;
 }
